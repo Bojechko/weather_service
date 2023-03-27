f02c882589389a93e0ebe023cd4eb8b6d0e32836
Bojechko <yapsyh@gmail.com>
2023-03-28 01:54:54 +0300

    done
    http://localhost:8080/v1/current/ww/moscow
    
    http://localhost:8080/v1/current/andrey/moscow
    
    http://localhost:8080/v1/forecast/andrey/moscow/2023-03-29
    
    http://localhost:8080/v1/forecast/ww/moscow/2023-03-29

--- src/main/java/com/example/nanay/controller/MessageController.java
+++ src/main/java/com/example/nanay/controller/MessageController.java
@@ -4,10 +4,12 @@ import com.example.nanay.gRPCClient.GRPCClient;
 import com.example.nanay.weather.WeatherAPI;
 import org.json.JSONObject;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.util.Date;
@@ -20,7 +22,7 @@ public class MessageController {
     @GetMapping("current/{login}/{city}")
     public String currentWeather(@PathVariable String login, @PathVariable String city) throws IOException, InterruptedException {
         if(!GRPCClient.makeRequest(login)) {
-            return "Неверный логин";
+            return String.valueOf(HttpStatus.FORBIDDEN);
         }
 
         double celsius = WeatherAPI.collectTemp(city) - 273.15;
@@ -37,8 +39,12 @@ public class MessageController {
         return json.toString();
     }
 
-    @GetMapping("forecast/{city}/{date}")
-    public String timestampWeather(@PathVariable String city, @PathVariable("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) throws IOException {
+    @GetMapping("forecast/{login}/{city}/{date}")
+    public String timestampWeather(@PathVariable String login, @PathVariable String city, @PathVariable("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) throws IOException, InterruptedException {
+
+        if(!GRPCClient.makeRequest(login)) {
+            return String.valueOf(HttpStatus.FORBIDDEN);
+        }
 
         long timestamp = date.getTime() / 1000;
         double celsius = WeatherAPI.collectTemp(city, timestamp) - 273.15;
